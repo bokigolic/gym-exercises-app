a6478d0e3aaad4fe583257cbe1e51583a548a464
bokigolic <bokigolic32@gmail.com>
2022-09-22 16:09:07 -0400

    add router, work on components

--- gym-app/src/App.js
+++ gym-app/src/App.js
@@ -1,24 +1,19 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import { Route, Routes } from 'react-router-dom'
+import { Box } from '@mui/material';
+import Home from './pages/Home'
+import ExerciseDetail from './pages/ExerciseDetail';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Box width="400px">
+      Navbar
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/excersise/:id" element={<ExerciseDetail />} />
+      </Routes>
+    </Box>
   );
 }
 
---
+++ gym-app/src/pages/ExerciseDetail.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+const ExerciseDetail = () => {
+  return (
+    <div>Vjezbaj pizda ti materina</div>
+  )
+};
+
+export default ExerciseDetail;
