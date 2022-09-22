c19a5c2ea8bf2e251619fd2b9da5c783ea6c5cd4
bokigolic <bokigolic32@gmail.com>
2022-09-22 16:22:58 -0400

    add BrowserRouter in root

--- gym-app/src/App.js
+++ gym-app/src/App.js
@@ -4,15 +4,17 @@ import { Route, Routes } from 'react-router-dom'
 import { Box } from '@mui/material';
 import Home from './pages/Home'
 import ExerciseDetail from './pages/ExerciseDetail';
+import Footer from './components/Footer';
 
 function App() {
   return (
-    <Box width="400px">
+    <Box width="400px" sx={{ width: { xl: '1488' } }} m="auto">
       Navbar
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/excersise/:id" element={<ExerciseDetail />} />
       </Routes>
+      <Footer />
     </Box>
   );
 }
