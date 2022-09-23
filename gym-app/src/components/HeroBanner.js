422703ec8636a767e643dd36c81cd9adfb83cbb2
bokigolic <bokigolic32@gmail.com>
2022-09-23 15:06:41 -0400

    use fetch data, work on exercises page

--- gym-app/src/components/HeroBanner.js
+++ gym-app/src/components/HeroBanner.js
@@ -8,17 +8,34 @@ const HeroBanner = () => {
     <Box sx={{ mt: { lg: '212px', xs: '70px' }, ml: { sm: '50px' } }} position="relative" p="20px">
       <Typography color="#FF2625" fontWeight="600" fontSize="26px">Fitness App</Typography>
       <Typography fontWeight={700} sx={{ fontSize: { lg: '44px', xs: '40px' } }} mb="23px" mt="30px">
-        Keep challenging <br />
+        Keep challenging
+        <br />
         yourself
       </Typography>
-      <Typography fontSize="22px" fontFamily="Alegreya" lineHeight="35px" mb={4}>
+      <Typography fontSize="22px"
+        fontFamily="Alegreya"
+        lineHeight="35px"
+        mb={4}>
         Check out the most effective exercises personalized to you
       </Typography>
-      <Button variant="contained" href="#exercises" sx={{padding:'10px'}}>Explore Exercises</Button>
-      <Typography fontWeight={600} color="blue" sx={{ opacity: '0.1', display: { lg: 'block', xs: 'none' }, fontSize: '180px' }}>
+      <Button variant="contained"
+        href="#exercises"
+        sx={{ padding: '10px' }}
+      >Explore Exercises
+      </Button>
+      <Typography fontWeight={600}
+        color="blue"
+        sx={{
+          opacity: '0.1',
+          display: { lg: 'block', xs: 'none' },
+          fontSize: '180px'
+        }}>
         Exercise
       </Typography>
-      <img src={HeroBannerImage} alt="hero-banner" className="hero-banner-img" />
+      <img src={HeroBannerImage}
+        alt="hero-banner"
+        className="hero-banner-img"
+      />
     </Box>
 
   )
