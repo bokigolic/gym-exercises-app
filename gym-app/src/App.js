8b349f06469b14a305f152f67ace7829096f32d0
bokigolic <bokigolic32@gmail.com>
2022-09-22 22:10:34 -0400

    add routes, logo and work on style

--- gym-app/src/App.js
+++ gym-app/src/App.js
@@ -4,12 +4,14 @@ import { Route, Routes } from 'react-router-dom'
 import { Box } from '@mui/material';
 import Home from './pages/Home'
 import ExerciseDetail from './pages/ExerciseDetail';
+import Navbar from './components/Navbar';
 import Footer from './components/Footer';
+
 
 function App() {
   return (
     <Box width="400px" sx={{ width: { xl: '1488' } }} m="auto">
-      Navbar
+      <Navbar />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/excersise/:id" element={<ExerciseDetail />} />
