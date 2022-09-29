3c0a65853bf21ba6cba00384b3efe5088fe17a37
bokigolic <bokigolic32@gmail.com>
2022-09-29 15:21:31 -0400

    Work on exercises Detail

--- gym-app/src/pages/ExerciseDetail.js
+++ gym-app/src/pages/ExerciseDetail.js
@@ -1,11 +1,20 @@
+import React, { useEffect, useState } from 'react';
+import { useParams } from 'react-router-dom';
+import { Box } from '@mui/material';
 
+import { exerciseOptions, fetchData, youtubeOptions } from '../utils/fetchData';
 
 const ExerciseDetail = () => {
   return (
-    <></>
+    <Box>
+      <Detal />
+      <ExercisesVideos />
+      <SimilarExercises />
+
+    </Box> 
   )
 
-    
+
 };
 
 export default ExerciseDetail;
