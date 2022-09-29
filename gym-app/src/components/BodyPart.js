cebf731ced2a0a65a6ba7c6cc4ce210e091fe7d9
bokigolic <bokigolic32@gmail.com>
2022-09-29 00:28:06 -0400

    work on slider

--- gym-app/src/components/BodyPart.js
+++ gym-app/src/components/BodyPart.js
@@ -1,4 +1,3 @@
-
 import React from 'react';
 import { Stack, Typography } from '@mui/material';
 import Icon from '../assets/icons/gym.png';
@@ -10,22 +9,31 @@ const BodyPart = ({ item, setBodyPart, bodyPart }) => (
     justifyContent="center"
     className="bodyPart-card"
     sx={{
-      borderTop: bodyPart === item ? '4px solid #ff2625' : '',
-      backgroundColor: '#fff',
+      borderTop: bodyPart === item ? '4px solid #FF2625' : "",
+      background: '#fff',
       borderBottomLeftRadius: '20px',
       width: '270px',
-      height: '280px',
+      height: '282px',
       cursor: 'pointer',
-      gap: '470px'
+      gap: '47px'
     }}
-    >
     onClick={() => {
       setBodyPart(item);
       window.scrollTo({ top: 1800, left: 100, behavior: 'smooth' });
     }}
-
-    <img src={Icon} alt="dumbbell" style={{ width: '40px', height: '40px' }} />
-    <Typography fontSize="24px" fontWeight="bold" fontFamily="Alegreya" color="#3A1212" textTransform="capitalize"> {item}</Typography>
+  >
+    <img src={Icon} alt="dumbbell"
+      style={{
+        width: '40px',
+        height: '40px'
+      }} />
+    <Typography
+      fontSize="24px"
+      fontWeight="bold"
+      fontFamily="Alegreya"
+      color="#3A1212"
+      textTransform="capitalize"> {item}
+    </Typography>
   </Stack>
 );
 
